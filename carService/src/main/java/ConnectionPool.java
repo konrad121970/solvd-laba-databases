966332fd79e05004d9c84fe4d2a32e92c72cc4f8
konrad121970 <konrad121970@windowslive.com>
2023-12-20 20:39:58 +0100

    Add ConnectionPool class

---
+++ carService/src/main/java/ConnectionPool.java
@@ -0,0 +1,85 @@
+import com.solvd.laba.config.Config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class ConnectionPool {
+    private static volatile ConnectionPool instance;
+    private final List<Connection> connectionList;
+
+    private ConnectionPool() {
+        try {
+            Class.forName(Config.DRIVER.getValue());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Unable to find driver class.", e);
+        }
+
+        int connectionPoolSize = Integer.parseInt(Config.POOL_SIZE.getValue());
+        this.connectionList = new ArrayList<>(connectionPoolSize);
+        IntStream.range(0, connectionPoolSize)
+                .boxed()
+                .forEach(index -> connectionList.add(createConnection()));
+    }
+
+    private Connection createConnection() {
+        Connection connection;
+
+        try {
+            connection = DriverManager.getConnection(Config.URL.getValue(), Config.USER.getValue(), Config.PASSWORD.getValue());
+        } catch (SQLException e) {
+            throw new RuntimeException("Unable to create connection.", e);
+        }
+        return connection;
+    }
+
+    public static ConnectionPool getInstance() {
+        if (instance == null) {
+            synchronized (ConnectionPool.class) {
+                if (instance == null) {
+                    instance = new ConnectionPool();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public synchronized Connection getConnection() {
+        while (connectionList.isEmpty()) {
+            try {
+                wait(); // Wait until a connection is released
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException("Interrupted while waiting for a connection.", e);
+            }
+        }
+
+        Connection connection = connectionList.remove(connectionList.size() - 1);
+        connection = createConnection();
+
+        return connection;
+    }
+
+    public synchronized void releaseConnection(Connection connection) {
+        synchronized (connectionList) {
+            connectionList.add(connection);
+            notify(); // Notify waiting threads that a connection is available
+        }
+    }
+
+    public synchronized void closeConnections() {
+        connectionList.forEach(connection -> {
+            try {
+                if (connection != null && !connection.isClosed()) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+        connectionList.clear();
+    }
+}
