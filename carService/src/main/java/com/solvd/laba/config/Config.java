addd1e1efc2a4ce4026c3270997be469a2fd673a
konrad121970 <konrad121970@windowslive.com>
2023-12-20 20:30:27 +0100

    Add config enum for config file

---
+++ carService/src/main/java/com/solvd/laba/config/Config.java
@@ -0,0 +1,40 @@
+package com.solvd.laba.config;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public enum Config {
+    DRIVER("driver"),
+    URL("url"),
+    USER("user"),
+    PASSWORD("password"),
+    POOL_SIZE("poolSize");
+
+    private final String key;
+    private static Properties properties;
+
+    static {
+        properties = new Properties();
+        try (InputStream input = Config.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new RuntimeException("Sorry, unable to find config.properties");
+            }
+            // load a properties file from class path, inside static method
+            properties.load(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    Config(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return properties.getProperty(key);
+    }
+}
