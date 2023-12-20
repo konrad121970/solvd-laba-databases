cb6d1e5f5bd3d2a4ffca4946d96cc6cfd03ba82c
konrad121970 <konrad121970@windowslive.com>
2023-12-20 15:30:23 +0100

    update update_script.sql

--- DATABASE_SCHEMA/scripts/delete_script.sql
+++ DATABASE_SCHEMA/scripts/delete_script.sql
@@ -16,10 +16,28 @@ WHERE customers.name = 'Ava' AND customers.surname = 'Clark';
 -- delete one customer 
 DELETE FROM employees WHERE name = 'John' AND surname = 'Doe';
 
-
-DELETE stocks, products
+-- delete stock with all its products
+DELETE s, shp, p
 FROM stocks s
 join stocks_has_products shp on shp.stocks_id = s.id
-LEFT JOIN products p ON stocks.id = p.stock_id
-WHERE stocks.name = 'Warehouse A';
+LEFT JOIN products p ON shp.products_id = p.id
+WHERE s.name = 'Warehouse';
+
+-- delete all vehicles with VIN number starting from '1'
+DELETE FROM vehicles WHERE vin LIKE '1%';
+
+-- delete all service orders with no invoice assigned
+DELETE FROM service_orders WHERE invoices_id IS NULL;
+
+-- delete all products not associated with any stock
+DELETE FROM products
+WHERE NOT EXISTS (SELECT 1 FROM stocks_has_products shp WHERE shp.products_id = products.id);
+
+-- delete all bonus payments with amount higher than 2000
+DELETE FROM bonus_payments WHERE amount > 2000;
+
+-- delete all invoices older than 1 year
+DELETE FROM invoices WHERE date_time < NOW() - INTERVAL 1 YEAR;
+
+
 
--- DATABASE_SCHEMA/scripts/update_script.sql
+++ DATABASE_SCHEMA/scripts/update_script.sql
@@ -1,19 +1,37 @@
-UPDATE stocks_has_products SET count = count + 5 WHERE stocks_id = 2 AND products_id = 1;
 
-UPDATE accounts SET login = 'john_doe_123' WHERE employees_id = 1;
+UPDATE stocks_has_products
+SET count = count + 5
+WHERE stocks_id = (SELECT id FROM stocks WHERE name = 'Shop')
+AND products_id = (SELECT id FROM products WHERE product_number = 'P002');
 
-UPDATE reoles_has_accounts SET roles_id = 2 WHERE accounts_id = 2 AND roles_id = 3;
+UPDATE roles
+SET name = 'Lead Mechanic'
+WHERE name = 'Mechanic';
 
-UPDATE roles SET name = 'Lead Mechanic' WHERE id = 3;
+UPDATE accounts
+SET password = 'new_hashed_password'
+WHERE employees_id = (SELECT id FROM employees WHERE surname = 'Doe');
 
-UPDATE accounts SET password = 'new_hashed_password' WHERE id = 1;
+UPDATE contracts
+SET active = 0
+WHERE employees_id = (SELECT id FROM employees WHERE surname = 'Doe');
 
-UPDATE contracts SET active = 0 WHERE employees_id = 1;
+UPDATE accounts
+SET login = 'john_doe_123'
+WHERE employees_id = (SELECT id FROM employees WHERE surname = 'Doe');
 
-UPDATE workshops SET name = 'Pablo\'s Auto Repair' WHERE id = 1;
+UPDATE workshops
+SET name = 'Auto garage'
+WHERE name = 'Pablo\'s Auto Repair';
 
-UPDATE adresses SET city = 'Warsaw', street = 'Main', postal_code = '20-123' WHERE id = 1;
+UPDATE adresses
+SET city = 'Warsaw', street = 'Main', postal_code = '20-123'
+WHERE city = 'Bialystok';
 
-UPDATE employees SET name = 'Henry' WHERE id = 2;
+UPDATE employees
+SET name = 'Henry'
+WHERE surname = 'Doe';
 
-UPDATE employees SET phone_number = '987654321' WHERE id = 1;
+UPDATE employees
+SET phone_number = '987654321'
+WHERE surname = 'Doe';
