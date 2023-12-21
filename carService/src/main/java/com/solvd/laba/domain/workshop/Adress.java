6446c9a834ed4908f389566dcbccc6f2a6e60862
konrad121970 <konrad121970@windowslive.com>
2023-12-21 03:39:17 +0100

    Implement AdressService

--- carService/src/main/java/com/solvd/laba/domain/workshop/Adress.java
+++ carService/src/main/java/com/solvd/laba/domain/workshop/Adress.java
@@ -6,4 +6,44 @@ public class Adress {
     private String street;
     private String buildingNumber;
     private String postalCode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public void setBuildingNumber(String buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 }
