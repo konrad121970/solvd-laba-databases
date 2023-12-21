43aa8c620f368b02c1e93a90737bb81578c5afe8
konrad121970 <konrad121970@windowslive.com>
2023-12-21 16:13:51 +0100

    add Role service

--- carService/src/main/java/com/solvd/laba/service/account/IRoleService.java
+++ carService/src/main/java/com/solvd/laba/service/account/IRoleService.java
@@ -1,4 +1,17 @@
 package com.solvd.laba.service.account;
 
+import com.solvd.laba.domain.account.Role;
+
+import java.util.List;
+
 public interface IRoleService {
+    void createRole(Role role);
+
+    Role getRoleById(Long id);
+
+    List<Role> getAllRoles();
+
+    void updateRole(Role role);
+
+    void deleteRole(Long id);
 }
--- carService/src/main/java/com/solvd/laba/service/account/impl/RoleService.java
+++ carService/src/main/java/com/solvd/laba/service/account/impl/RoleService.java
@@ -1,4 +1,47 @@
 package com.solvd.laba.service.account.impl;
 
-public class RoleService {
+import com.solvd.laba.domain.account.Role;
+import com.solvd.laba.persistence.account.IRoleDAO;
+import com.solvd.laba.service.account.IRoleService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.lang.invoke.MethodHandles;
+import java.util.List;
+
+public class RoleService implements IRoleService {
+    private static final Logger LOGGER = LogManager.getLogger(MethodHandles.lookup().lookupClass());
+    private final IRoleDAO roleDAO;
+
+    public RoleService(IRoleDAO roleDAO) {
+        this.roleDAO = roleDAO;
+    }
+
+    @Override
+    public void createRole(Role role) {
+        roleDAO.create(role);
+        LOGGER.info("Role created: {}", role);
+    }
+
+    @Override
+    public Role getRoleById(Long id) {
+        return roleDAO.getById(id);
+    }
+
+    @Override
+    public List<Role> getAllRoles() {
+        return roleDAO.getAll();
+    }
+
+    @Override
+    public void updateRole(Role role) {
+        roleDAO.update(role);
+        LOGGER.info("Role updated: {}", role);
+    }
+
+    @Override
+    public void deleteRole(Long id) {
+        roleDAO.delete(id);
+        LOGGER.info("Role deleted with id: {}", id);
+    }
 }
