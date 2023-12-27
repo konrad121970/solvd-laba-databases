a645f7e376c3fe0f717e18894ae0e59a4883e2db
konrad121970 <konrad121970@windowslive.com>
2023-12-27 02:28:53 +0100

    Start implementing ServiceOrderDAO

--- carService/src/main/java/com/solvd/laba/persistence/order/impl/ServiceOrderDAO.java
+++ carService/src/main/java/com/solvd/laba/persistence/order/impl/ServiceOrderDAO.java
@@ -1,9 +1,9 @@
 package com.solvd.laba.persistence.order.impl;
 
-import com.solvd.laba.persistence.ConnectionPool;
-import com.solvd.laba.persistence.order.IServiceOrderDAO;
 import com.solvd.laba.domain.order.Invoice;
 import com.solvd.laba.domain.order.ServiceOrder;
+import com.solvd.laba.persistence.ConnectionPool;
+import com.solvd.laba.persistence.order.IServiceOrderDAO;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,10 +19,61 @@ public class ServiceOrderDAO implements IServiceOrderDAO {
     private static final Logger LOGGER = LogManager.getLogger(MethodHandles.lookup().lookupClass());
     private static final ConnectionPool CONNECTION_POOL = ConnectionPool.getInstance();
     private static final String CREATE_QUERY = "INSERT INTO service_orders (date, completed, description, invoice_id) VALUES (?, ?, ?, ?)";
-    private static final String GET_BY_ID_QUERY = "SELECT * FROM service_orders WHERE id = ?";
+    private static final String GET_BY_ID_QUERY =
+            "SELECT " +
+                    "   so.id AS service_order_id, so.date AS service_order_date, so.completed AS service_order_completed, " +
+                    "   so.description AS service_order_description, i.id as invoice_id, i.date_time as invoice_date_time, i.total_price as invoice_total_price, " +
+                    "   p.product_number as product_number, p.name as product_name, p.price as product_price " +
+                    "FROM service_orders so " +
+                    "LEFT JOIN invoices i ON so.invoices_id = i.id " +
+                    "LEFT JOIN invoices_has_products ihp ON ihp.invoices_id = i.id " +
+                    "LEFT JOIN products p ON p.id = ihp.products_id " +
+                    "WHERE so.id = ?";
     private static final String GET_ALL_QUERY = "SELECT * FROM service_orders";
     private static final String UPDATE_QUERY = "UPDATE service_orders SET date = ?, completed = ?, description = ?, invoice_id = ? WHERE id = ?";
     private static final String DELETE_QUERY = "DELETE FROM service_orders WHERE id = ?";
+
+    public static List<ServiceOrder> mapRow(ResultSet resultSet, List<ServiceOrder> serviceOrders) throws SQLException {
+        if (serviceOrders == null) {
+            serviceOrders = new ArrayList<>();
+        }
+
+        Long id = resultSet.getLong("service_order_id");
+
+        if (id != 0) {
+            if (!serviceOrders.stream().anyMatch(so -> so.getId() == id)) {
+
+                ServiceOrder serviceOrder = findById(id, serviceOrders);
+                serviceOrder.setDescription(resultSet.getString("service_order_description"));
+                serviceOrder.setCompleted(resultSet.getBoolean("service_order_completed"));
+                serviceOrder.setDate(resultSet.getDate("service_order_date"));
+
+                // serviceOrders.add(serviceOrder);
+
+            }
+
+        /*    Invoice invoice = InvoiceDAO.mapInvoice(resultSet);
+            if (invoice != null) {
+                serviceOrder.setInvoice(invoice);
+            }*/
+
+
+        }
+
+        return serviceOrders;
+    }
+
+    private static ServiceOrder findById(Long id, List<ServiceOrder> serviceOrders) {
+        return serviceOrders.stream()
+                .filter(vehicle -> vehicle.getId().equals(id))
+                .findFirst()
+                .orElseGet(() -> {
+                    ServiceOrder newServiceORder = new ServiceOrder();
+                    newServiceORder.setId(id);
+                    serviceOrders.add(newServiceORder);
+                    return newServiceORder;
+                });
+    }
 
     @Override
     public void create(ServiceOrder serviceOrder) {
@@ -51,14 +102,14 @@ public class ServiceOrderDAO implements IServiceOrderDAO {
     @Override
     public ServiceOrder getById(Long id) {
         Connection connection = CONNECTION_POOL.getConnection();
-        ServiceOrder serviceOrder = null;
+        List<ServiceOrder> serviceOrders = new ArrayList<>();
         try (PreparedStatement preparedStatement = connection.prepareStatement(GET_BY_ID_QUERY)) {
             preparedStatement.setLong(1, id);
 
             ResultSet result = preparedStatement.executeQuery();
 
             if (result.next()) {
-                serviceOrder = mapServiceOrder(result);
+                serviceOrders = mapRow(result, serviceOrders);
             }
         } catch (SQLException e) {
             LOGGER.error(e.getMessage());
@@ -66,7 +117,7 @@ public class ServiceOrderDAO implements IServiceOrderDAO {
             CONNECTION_POOL.releaseConnection(connection);
         }
 
-        return serviceOrder;
+        return serviceOrders.get(0);
     }
 
     @Override
