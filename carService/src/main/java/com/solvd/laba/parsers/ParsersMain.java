7c10ef45ec7087731861492656b3c0a7c2dd6212
konrad121970 <konrad121970@windowslive.com>
2024-01-05 13:49:44 +0100

    Add xml validation by xsd schema

--- carService/src/main/java/com/solvd/laba/parsers/ParsersMain.java
+++ carService/src/main/java/com/solvd/laba/parsers/ParsersMain.java
@@ -2,29 +2,58 @@ package com.solvd.laba.parsers;
 
 
 import com.solvd.laba.parsers.model.Employee;
-import com.solvd.laba.parsers.sax.EmployeeParser;
+import com.solvd.laba.parsers.sax.XMLEmployeeParser;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 import java.io.File;
 import java.io.IOException;
+import java.lang.invoke.MethodHandles;
 
 public class ParsersMain {
+    private static final Logger LOGGER = LogManager.getLogger(MethodHandles.lookup().lookupClass());
+
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+
+        String xmlFilePath = "src/main/resources/parsers/xml/employee.xml";
+        String xsdFilePath = "src/main/resources/parsers/xml/employee.xsd";
+
+        try {
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+            Schema schema = schemaFactory.newSchema(new StreamSource(xsdFilePath));
+
+            Validator validator = schema.newValidator();
+
+            validator.validate(new StreamSource(xmlFilePath));
+
+            LOGGER.info("XML validation success");
+
+        } catch (SAXException e) {
+            LOGGER.error("Validation failed: " + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            LOGGER.error("Error reading XML file: " + e.getMessage());
+            e.printStackTrace();
+        }
+
 
         SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
         SAXParser saxParser = saxParserFactory.newSAXParser();
+        XMLEmployeeParser XMLEmployeeParser = new XMLEmployeeParser();
+        saxParser.parse(new File(xmlFilePath), XMLEmployeeParser);
 
-        String xml = "src/main/resources/parsers/xml/employee.xml";
+        Employee employee = XMLEmployeeParser.getEmployee();
 
-        EmployeeParser employeeParser = new EmployeeParser();
-
-        saxParser.parse(new File(xml), employeeParser);
-
-        Employee employee = employeeParser.getEmployee();
-
-        System.out.println("lol");
+        System.out.println("test");
     }
 }
--- carService/src/main/java/com/solvd/laba/parsers/sax/XMLEmployeeParser.java
+++ carService/src/main/java/com/solvd/laba/parsers/sax/XMLEmployeeParser.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EmployeeParser extends DefaultHandler {
+public class XMLEmployeeParser extends DefaultHandler {
     private StringBuilder currentElementValue;
     private Employee employee;
     private Contract currentContract;
